a477b1c693fc60b941539d4ef43cd8fce1805c7a
Christer Engman <christer.engman@hm.com>
2022-12-04 20:58:35 +0100

    Day 03-2 PS

--- day03/Day03.ps1
+++ day03/Day03.ps1
@@ -5,8 +5,10 @@ function Get-Priority {
   [byte] $c - ($c -cmatch "[a-z]" ? 96 : 38)
 }
 
+$group = 0..2
+
 Get-Content (Join-Path $PSScriptRoot "input.txt") |
-ForEach-Object { $sum = 0 } {
+ForEach-Object { $sum = 0; $sum2 = 0; $i = 0 } {
   $size = $_.Length -shr 1
   $first = $_.Substring(0, $size)
   $second = $_.Substring($size)
@@ -18,4 +20,19 @@ ForEach-Object { $sum = 0 } {
       break
     }
   }
-} { $sum }
+
+  $group[$i] = $_
+
+  if ($i -eq 2) {
+    foreach ($c in $group[0].ToCharArray()) {
+      if ($group[1].Contains($c) -and $group[2].Contains($c)) {
+        $sum2 += Get-Priority $c
+
+        break
+      }
+    }
+  }
+
+  $i = ($i + 1) % 3
+
+} { $sum; $sum2 }
